e9a106fdfe8a4613d675e200bd064c9be9b51da0
runrin <run.rin@rin.run>
2020-10-19 23:52:39 -0700

    usable, checking for errors

--- tt.c
+++ tt.c
@@ -4,6 +4,9 @@
 #include <ctype.h>
 #include <ncurses.h>
 
+#define MAX_LINES 255
+#define MAX_LINE_LENGTH 81
+
 /*
  * TODO load each line of the document in as a char array, and get user input
  * one key at a time, then step along the char array, check if the char is
@@ -15,58 +18,65 @@
 
 int main(int argc, char *argv[])
 {
+        char buf[MAX_LINES][MAX_LINE_LENGTH];
+        int lc, bad, good;
+        FILE *fp;
         char *test;
         test = (char *) malloc(81);
-        int lines;
 
-        initscr();			/* Start curses mode 		*/
-	raw();				/* Line buffering disabled	*/
-	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
-        start_color();                  /* Start color                  */
-
-        char s;
-        FILE *fp;
         fp=fopen(argv[1], "r");
 
-        lines = 0;
-        while ((s=getc(fp)) != EOF) {
-                if (s == '\n')
-                        lines++;
+        lc = 0;
+        while (fgets(buf[lc], MAX_LINE_LENGTH, fp) && lc < MAX_LINES - 1) {
+                buf[lc][strlen(buf[lc]) - 1] = '\0';
+                lc++;
         }
         fclose(fp);
- 
 
-        fp=fopen(argv[1], "r");
-        int i = 0;
-        while (i < lines) {
-                int yes = 1;
-                while ((s=getc(fp)) != '\n') {
-                        if (isascii(s)) {
-                                addch(s);
-                                yes = 1;
+        initscr(); 			/* Start curses mode 		*/
+	raw(); 				/* Line buffering disabled	*/
+	keypad(stdscr, TRUE); 		/* We get F1, F2 etc..		*/
+        noecho();
+        start_color();                  /* Start color                  */
+        init_pair(1, COLOR_RED, COLOR_BLACK);
+
+        bad = 0;
+        good = 0;
+        for(int i = 0; i < lc; i++) {
+                printw("%s\n", buf[i]);
+                int ch;
+                for (int j = 0; j < (int)strlen(buf[i]); j++) {
+                       ch = getch();
+                       if (ch == KEY_F(4))
+                               goto end;
+                       else if (ch == KEY_BACKSPACE) {
+                                j = j - 2;
+                                move(1,j + 1);
+                                addch(' ');
+                                move(1,j + 1);
                         }
-                        else if (yes) {
-                                addch('-');
-                                yes = 0;
+                        else if (ch == buf[i][j]) {
+                                addch(ch);
+                                good++;
+                        }
+                        else {
+                                attron(COLOR_PAIR(1));
+                                addch(ch);
+                                attroff(COLOR_PAIR(1));
+                                bad++;
                         }
                 }
-                move(1,0);
-                getstr(test);
-
                 move(0,0);
-                for (int i = 0; i < 80; i++)
+                for (int j = 0; j <= 80; j++)
                         addch(' ');
-
                 move(1,0);
-                for (int i = 0; i < 80; i++)
+                for (int j = 0; j <= 80; j++)
                         addch(' ');
-
                 move(0,0);
-                i++;
-                if (strcmp(test, ":q") == 0)
-                        break;
         }
 
-        fclose(fp);
+end:
         endwin();
+        printf("%d mistakes, %d total characters\n", bad, good - bad);
+        return 0;
 }
