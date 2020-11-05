94b2f4c15a83e5d4f097d0a8539321089eeeaace
runrin <run.rin@rin.run>
2020-11-04 16:00:37 -0800

    renamed variables to be clearer, narrowed scope of some variables, better comments, general cleanup.
    text typed correctly now shows green.

--- tt.c
+++ tt.c
@@ -6,28 +6,30 @@
 #include <ncurses.h>
 
 #define MAX_LINES 255
-#define MAX_LINE_LENGTH 81
+#define MAX_LINE_LENGTH 80
 
 int main(int argc, char *argv[])
 {
-        char buf[MAX_LINES][MAX_LINE_LENGTH];
-        char line_to_check[MAX_LINE_LENGTH];
-        int lc, bad, good, space;
-        FILE *fp;
-        float total_words, total_minutes;
-
+        char buf[MAX_LINES][MAX_LINE_LENGTH + 1];
+        char input_string[MAX_LINE_LENGTH + 1];
+        int buf_lines, typed_char_count;
+        float typed_word_count, total_minutes;
         time_t start_time, end_time;
 
 
         /* open the file, load its lines into an array of strings, close file */
-        fp=fopen(argv[1], "r");
-
-        lc = 0;
-        while (fgets(buf[lc], MAX_LINE_LENGTH, fp) && lc < MAX_LINES - 1) {
-                buf[lc][strlen(buf[lc]) - 1] = '\0';
-                lc++;
+        /* error handling */
+        {
+                FILE *fp;
+                fp=fopen(argv[1], "r");
+                buf_lines = 0;
+                while (fgets(buf[buf_lines], MAX_LINE_LENGTH + 1, fp) &&
+                                buf_lines < MAX_LINES - 1) {
+                        buf[buf_lines][strlen(buf[buf_lines]) - 1] = '\0';
+                        buf_lines++;
+                }
+                fclose(fp);
         }
-        fclose(fp);
 
         /* start ncurses screen */
         initscr();
@@ -36,30 +38,30 @@ int main(int argc, char *argv[])
         noecho();
         start_color();
         init_pair(1, COLOR_RED, COLOR_BLACK);
+        init_pair(2, COLOR_GREEN, COLOR_BLACK);
 
         /* add some initial information */
-        move(4,0);
-        printw("Press F4 to quit at any time.");
-        move(3,73);
+        move(5, 0);
+        printw("To quit press F4 at any time...");
+        move(3, MAX_LINE_LENGTH - 7);
         printw("--- WPM\n");
-        move(0,0);
+        move(0, 0);
 
-        /* get the current time */
+        /* get the start time */
         time(&start_time);
 
-        /* TODO remove all extra variables, there are a bunch */
-        bad = 0;
-        good = 0;
-        space = 0;
+        typed_char_count = 0;
         /* loop this for every line that has been loaded into the buffer */
-        for(int i = 0; i < lc; i++) {
-                printw("%s\n", buf[i]);
-                int ch;
+        for(int buf_index = 0; buf_index < buf_lines; buf_index++) {
+                int input_index;
+                /* print the current line from the file */
+                printw("%s\n", buf[buf_index]);
 
-                int j = 0;
+                input_index = 0;
                 /* until the input string matches the buffer string, do not
                  * move on to the next one */
-                while (strcmp(line_to_check, buf[i])) {
+                while (strcmp(input_string, buf[buf_index])) {
+                        int ch;
                         
                         /* get the input char */
                         ch = getch();
@@ -72,23 +74,25 @@ int main(int argc, char *argv[])
 
                         /* if backspace is pressed,
                          * and we're not at the start of the line */
-                        else if (ch == KEY_BACKSPACE && j > 0) {
-                                j--;
-                                move(1, j);
+                        else if (ch == KEY_BACKSPACE && input_index > 0) {
+                                input_index--;
+                                move(1, input_index);
                                 addch(' ');
-                                move(1, j);
+                                move(1, input_index);
                                 time(&end_time);
-                                line_to_check[j] = '\0';
+                                input_string[input_index] = '\0';
                         }
                         /* input character is correct */
-                        else if (ch == buf[i][j]) {
+                        else if (ch == buf[buf_index][input_index]) {
+                                attron(COLOR_PAIR(2));
                                 addch(ch);
-                                line_to_check[j] = ch;
+                                input_string[input_index] = ch;
                                 /* always add null after the current position
                                  * just in case we are at the end of the line */
-                                line_to_check[j + 1] = '\0';
+                                input_string[input_index + 1] = '\0';
                                 time(&end_time);
-                                j++;
+                                input_index++;
+                                attroff(COLOR_PAIR(2));
                         }
 
                         /* TODO account for special key presses, etc,
@@ -103,43 +107,43 @@ int main(int argc, char *argv[])
                                  * incorrectly placed, so that it is clear
                                  * that it is an error */
                                 if (ch == ' ')
-                                        addch('.');
+                                        addch('_');
                                 else
                                         addch(ch);
 
-                                line_to_check[j] = ch;
-                                line_to_check[j + 1] = '\0';
+                                input_string[input_index] = ch;
+                                input_string[input_index + 1] = '\0';
                                 attroff(COLOR_PAIR(1));
-                                j++;
+                                input_index++;
                         }
                 }
 
                 /* check total number of non-space characters in the line */
-                for (int k = 0; k < (int)strlen(line_to_check); k++) {
-                        if(line_to_check[k] != ' ')
-                                good++;
+                for (int i = 0; i < (int)strlen(input_string); i++) {
+                        if(input_string[i] != ' ')
+                                typed_char_count++;
                 }
 
                 /* check the end time after each line */
                 time(&end_time);
                 /* clear the screen for the next line */
-                for (int j = 0; j <= MAX_LINE_LENGTH; j++) {
-                        for (int k = 0; k <= 3; k ++) {
-                                move(k,j);
+                for (int x = 0; x <= MAX_LINE_LENGTH ; x++) {
+                        for (int y = 0; y <= 3; y ++) {
+                                move(y, x);
                                 addch(' ');
                         }
                 }
 
                 /* calculate total words typed, and ellapsed time since starting
                  * the program */
-                total_words = (good / 5.0);
+                typed_word_count = (typed_char_count / 5.0);
                 total_minutes = ((end_time - start_time) / 60.0);
 
                 /* print the cumulative WPM since starting the program */
-                move(3,73);
-                printw("%3.0f WPM\n", (total_words / total_minutes));
+                move(3, MAX_LINE_LENGTH - 7);
+                printw("%3.0f WPM\n", (typed_word_count / total_minutes));
 
                 /* reset position for next string */
-                move(0,0);
+                move(0, 0);
         }
 }
